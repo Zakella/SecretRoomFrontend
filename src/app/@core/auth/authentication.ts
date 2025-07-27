65e9207a703050e5863aa0d5ed4a3fabd586ec36
Nikolay Kiritsa <@nkiritsa>
2025-07-27 20:11:34 +0300

    added new components

---
+++ src/app/@core/auth/authentication.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {BehaviorSubject, Observable, Subscriber, tap} from 'rxjs';
+import {UserDetails} from '../entities/user-details';
+import {User} from '../entities/user';
+import {HttpClient} from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Authentication {
+  private jwtHelper = new JwtHelperService();
+  loggedIn = new BehaviorSubject<boolean>(this.hasValidTokenInLocalStorage());
+
+  constructor(private http: HttpClient) { }
+
+  isLoggedIn(): Observable<boolean> {
+    return new Observable<boolean>(subscriber => {
+      if (this.hasValidTokenInLocalStorage()) {
+        this.notifyLoggedInStatus(true, subscriber);
+        console.log("Its ok!" + new Date());
+      } else {
+        console.log("Auto logout" + new Date());
+        this.notifyLoggedInStatus(false, subscriber);
+        localStorage.removeItem('user');
+      }
+      subscriber.complete();
+    });
+  }
+
+  private hasValidTokenInLocalStorage(): boolean {
+    if (this.isBrowser()) {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser && storedUser !== "null") {
+        const userDetails: UserDetails = JSON.parse(storedUser);
+        const token = userDetails.accessToken;
+        return Boolean(token) && !this.jwtHelper.isTokenExpired(token);
+      }
+    }
+    return false;
+  }
+
+  private notifyLoggedInStatus(isLoggedIn: boolean, subscriber: Subscriber<boolean>) {
+    this.loggedIn.next(isLoggedIn);
+    subscriber.next(isLoggedIn);
+  }
+
+  registration(user: User): Observable<UserDetails> {
+    const url = environment.apiUrl  + 'v1/users';
+    return this.http.post<UserDetails>(url, user).pipe(
+      tap((userDetails: UserDetails) => {
+        this.loggedIn.next(true);
+      })
+    );
+  }
+
+  login(user: User): Observable<UserDetails> {
+    const url = environment.apiUrl + 'v1/users/login';
+    return this.http.post<UserDetails>(url, user).pipe(
+      tap(() => {
+        this.loggedIn.next(true);
+      })
+    );
+  }
+
+  logout(): void {
+    const userDetails = this.getUserDetails();
+    if (userDetails) {
+      const token = userDetails.accessToken;
+      const refreshToken = userDetails.refreshToken;
+      if (token && refreshToken) {
+        this.http.post(environment.apiUrl + 'v1/users/logout', {
+          refresh_token: refreshToken
+        }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }).subscribe(() => {
+            localStorage.removeItem('user');
+            this.loggedIn.next(false);
+          });
+      }
+    }
+  }
+
+  getUserDetails(): UserDetails | null {
+    if (this.isBrowser()) {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser && storedUser !== "null") {
+        return JSON.parse(storedUser);
+      }
+    }
+    return null;
+  }
+
+  restorePassword(email: string, lang: string): Observable<any> {
+    const url = environment.apiUrl +'v1/users/restore-password'; // Replace with your restore password API endpoint from UserController
+    return this.http.get(url, { params: { email, lang } });
+  }
+
+  resetPassword(token: string, newPassword: string): Observable<void> {
+    const url = environment.apiUrl +'v1/users/reset-password'; // Replace with your reset password API endpoint from UserController
+    return this.http.post<void>(url, { token, newPassword });
+  }
+
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+}
