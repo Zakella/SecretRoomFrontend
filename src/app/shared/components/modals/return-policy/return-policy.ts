5be64c126a030f1b7e080d103728ac0f7dcea0b7
Nikolay Kiritsa <@nkiritsa>
2025-12-16 17:44:48 +0200

    removed ButtonDirective from return-policy component; streamlined best-sellers component layout and styles; updated color scheme and hover effects in best-sellers.scss; adjusted text-slider color for improved readability

--- src/app/shared/components/modals/return-policy/return-policy.ts
+++ src/app/shared/components/modals/return-policy/return-policy.ts
@@ -1,12 +1,10 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TranslocoPipe} from '@ngneat/transloco';
 import {Dialog} from 'primeng/dialog';
-import {PrimeTemplate} from 'primeng/api';
-import {ButtonDirective} from 'primeng/button';
 
 @Component({
   selector: 'app-return-policy',
-  imports: [TranslocoPipe, Dialog, PrimeTemplate, ButtonDirective],
+  imports: [TranslocoPipe, Dialog],
   templateUrl: './return-policy.html',
   styleUrl: './return-policy.scss'
 })
