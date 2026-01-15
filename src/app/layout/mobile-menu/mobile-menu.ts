99ef6c761fa73311cc3002b4c8240cda81f7a637
Nikolay Kiritsa <@nkiritsa>
2026-01-15 11:44:19 +0200

    updated mobile-menu with transloco pipe for placeholder translation and improved drawer styles; replaced static images in categories list with dynamic URLs; enhanced category icons with rounded styling and consistent sizing; refined layout and encapsulation with ViewEncapsulation.None for global style adjustments

--- src/app/layout/mobile-menu/mobile-menu.ts
+++ src/app/layout/mobile-menu/mobile-menu.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, ViewEncapsulation} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 import {Language} from '../../@core/services/language';
 import {CartUi} from '../../shared/components/cart/services/cart';
 import {DrawerModule} from 'primeng/drawer';
 import {ButtonModule} from 'primeng/button';
 import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {TranslocoPipe} from '@ngneat/transloco';
 
 @Component({
   selector: 'mobile-menu',
@@ -12,10 +14,13 @@ import {NgForOf} from '@angular/common';
     RouterLink,
     DrawerModule,
     ButtonModule,
-    NgForOf
+    NgForOf,
+    FormsModule,
+    TranslocoPipe
   ],
   templateUrl: './mobile-menu.html',
-  styleUrl: './mobile-menu.scss'
+  styleUrl: './mobile-menu.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class MobileMenu {
   public router = inject(Router);
@@ -26,14 +31,12 @@ export class MobileMenu {
   public cartCount =  this.cartService.cartCount;
   visible: boolean = false;
   categories = [
-    { label: 'Skincare', icon: '/assets/icons/skincare.png' },
-    { label: 'Make Up', icon: '/assets/icons/makeup.png' },
-    { label: 'Perfumes', icon: '/assets/icons/perfume.png' },
-    { label: 'Hair', icon: '/assets/icons/hair.png' },
-    { label: 'Manicure & Pedicure', icon: '/assets/icons/nails.png' },
-    { label: 'Hygiene', icon: '/assets/icons/hygiene.png' },
-    { label: 'Accessories', icon: '/assets/icons/accessories.png' },
-    { label: 'Devices', icon: '/assets/icons/devices.png' }
+    { label: 'Skincare', icon: 'https://www.skincenterofsouthmiami.com/wp-content/uploads/2018/06/Skin-Center-of-South-Miami-Facials-and-Skin-Care.jpg' },
+    { label: 'Make Up', icon: 'https://www.celinesbeautyrooms.ie/wp-content/uploads/2023/02/make-up.jpg' },
+    { label: 'Perfumes', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0dBLgwZB1yZ0vMt05UuUDXZ8SIEODYpJArQ&s' },
+    { label: 'Hair', icon: 'https://lovebeautyandplanet.in/cdn/shop/articles/Facts_abou_hair_growth_you_should_know_500.jpg?v=1708085249' },
+    { label: 'Hygiene', icon: 'https://www.purifas.com/cdn/shop/articles/salon_hygiene_600x1000_58c72a6c-b672-473c-a9bc-13cbb46ff27d_1000x.png?v=1710804691' },
+    { label: 'Accessories', icon: 'https://img.freepik.com/premium-photo/collection-fashion-accessories-like-sunglasses-watches-necklaces-white-background_1271419-10666.jpg?semt=ais_hybrid&w=740&q=80' },
   ];
 
 
