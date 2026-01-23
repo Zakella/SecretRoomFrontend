37758adab474fe5b0cf6c72b800f32a4b64f6ee9
Nikolay Kiritsa <@nkiritsa>
2026-01-23 15:08:50 +0200

    added chat widget with toggleable mini-chat and message exchange; integrated responsive styles and animations; updated main-layout to include chat component; refined cookie-banner component with modal animations, local storage handling, and improved actions layout

--- src/app/layout/cookie-banner/cookie-banner.ts
+++ src/app/layout/cookie-banner/cookie-banner.ts
@@ -1,5 +1,6 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit, PLATFORM_ID, signal} from '@angular/core';
 import {Cookie} from '../../@core/services/cookie';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-cookie-banner',
@@ -7,11 +8,33 @@ import {Cookie} from '../../@core/services/cookie';
   templateUrl: './cookie-banner.html',
   styleUrl: './cookie-banner.scss'
 })
-export class CookieBanner {
+export class CookieBanner implements OnInit{
   private cookieService = inject(Cookie);
   accepted = this.cookieService.accepted;
 
   accept() {
     this.cookieService.acceptCookies();
+  }
+
+  showBanner = signal(true);
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // проверяем только на клиенте
+      const accepted = localStorage.getItem('cookiesAccepted') === 'true';
+      if (accepted) this.showBanner.set(false);
+    }
+  }
+
+  acceptCookies() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('cookiesAccepted', 'true');
+    }
+    this.showBanner.set(false);
+  }
+
+  openSettings() {
+    alert('Здесь можно открыть модальное окно с настройками cookies');
   }
 }
