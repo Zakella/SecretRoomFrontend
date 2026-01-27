c37f4830632cf2c0323a678d37f3d6f7187d4c5c
Nikolay Kiritsa <@nkiritsa>
2026-01-27 11:39:19 +0200

    refined header styles and HTML structure: improved layout for better alignment and spacing, updated color scheme for consistency, enhanced transitions and hover effects, and restructured dropdown and search bar for improved usability and clarity

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,15 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
-import { importProvidersFrom } from '@angular/core';
 
-const bootstrap = (context?: any) =>
-  bootstrapApplication(App, {
-    ...config,
-    providers: [
-      ...(config.providers || []),
-      { provide: 'SSR_CONTEXT', useValue: context } // передаем context через DI
-    ]
-  });
+
+const bootstrap = (context?: any) => bootstrapApplication(App, config, context);
+
 
 export default bootstrap;
