a3c56ef4d424ee18179a738683e3ca9fc9bc0c1d
zakella <zapolski.slava@gmail.com>
2026-02-02 13:58:51 +0200

    updated header navigation and styles: changed default language to Romanian, added icons for navigation items, and enhanced styling for better user experience

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -39,7 +39,7 @@ export const appConfig: ApplicationConfig = {
     provideTransloco({
       config: {
         availableLangs: ['ro', 'ru'],
-        defaultLang: 'ru',
+        defaultLang: 'ro',
         reRenderOnLangChange: true,
         prodMode: !isDevMode(),
       },
